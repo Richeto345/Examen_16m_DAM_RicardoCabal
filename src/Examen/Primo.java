76e55a71ead8fce3f8757b297ff7bfca5dbf8b1c
ticha <ticha@Ricardo>
2024-05-22 12:12:06 +0200

    Primer codigo

---
+++ src/Examen/Primo.java
@@ -0,0 +1,15 @@
+package Examen;
+import java.util.Scanner;
+public class Primo {
+
+	public static void main(String[] arg) {
+		int numero=0;
+		Scanner ent = new Scanner(System.in);
+		System.out.println("Buenos dias, soy Ricardo");
+		System.out.println("Introduce un numero para ver si es primo o no");
+		numero=ent.nextInt();
+		
+	}
+	
+	
+}
